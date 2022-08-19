4e665e459bcb4e311550aabf4140d8a4ac9d6df0
Chengzi <153680858@qq.com>
2022-08-19 23:10:14 +0800

    c#关于chart简单实践

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Timers.Timer();
+            this.timer2 = new System.Timers.Timer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(386, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "笨蛋姚沁怡率先出击\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(51, 152);
+            this.chart1.Margin = new System.Windows.Forms.Padding(0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "数据值";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(550, 180);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.SynchronizingObject = this;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.SynchronizingObject = this;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(652, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(464, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 388);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+
+        private System.Timers.Timer timer1;
+        private System.Timers.Timer timer2;
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Timer = System.Timers.Timer;
+
+namespace WindowsFormsApp1
+{
+    
+    public partial class Form1 : Form
+    {
+        /**
+         * 纵坐标list
+         */
+        static List<double> yList = new List<double>();
+        
+        /**
+         * 横坐标list
+         */
+        static List<String> xList = new List<String>();
+        static Random rnd = new Random();
+        static Timer timer = new Timer();
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化图表
+            initChart();
+            initComboBoxData();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //启动定时器
+            timer = new Timer(1000);  
+            timer.Elapsed += timer_Task;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            timer.Start();
+        }
+        
+        /**
+         * 定时器任务
+         */
+        private void timer_Task(object sender, EventArgs e)
+        {
+            //移除第一个横坐标
+            xList.RemoveAt(0);
+            //获取最新时间并添加列表尾
+            xList.Add(System.DateTime.Now.ToString("T"));
+            //移除第一个纵坐标
+            yList.RemoveAt(0);
+            //构造新的纵坐标值
+            yList.Add(getDataByBaseline(10));
+            //数据与坐标轴绑定
+            chart1.Series[0].Points.DataBindXY(xList, yList);
+        }
+        
+        /**
+         * 平滑数据构造
+         */
+        private double getDataByBaseline(int baseLine)
+        {
+            return rnd.NextDouble()*2.0 + baseLine - rnd.NextDouble()*4.0;
+        }
+
+        /**
+         * chart初始化
+         */
+        private void initChart()
+        {
+            // 构造初始值
+            for (int i = 0; i < 5; i++)
+            {
+                yList.Add(getDataByBaseline(10));
+                xList.Add(DateTime.Now.ToString("T"));
+            }
+            //修改横纵坐标类型
+            chart1.Series[0].XValueType = ChartValueType.Time;
+            chart1.Series[0].YValueType = ChartValueType.Double;   
+        }
+
+        private void initComboBoxData()
+        {
+            Object[] itmes = new object[12];
+            itmes[0] = "柱状图";
+            itmes[1] = "折线图";
+            itmes[2] = "饼图";
+            itmes[3] = "面积图";
+            itmes[4] = "气泡图";
+            itmes[5] = "K 线图";
+            itmes[6] = "条形图";
+            itmes[7] = "圆环图";
+            itmes[8] = "漏斗图";
+            itmes[9] = "卡吉图";
+            itmes[10] = "点图";
+            itmes[11] = "快速扫描线图";
+            comboBox1.Items.AddRange(itmes);
+            comboBox1.SelectedIndex = 10;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    chart1.Series[0].ChartType = SeriesChartType.Column;
+                    break;
+                case 1:
+                    chart1.Series[0].ChartType = SeriesChartType.Line;
+                    break;
+                case 2:
+                    chart1.Series[0].ChartType = SeriesChartType.Pie;
+                    break;
+                case 3:
+                    chart1.Series[0].ChartType = SeriesChartType.Area;
+                    break;
+                case 4:
+                    chart1.Series[0].ChartType = SeriesChartType.Bubble;
+                    break;
+                case 5:
+                    chart1.Series[0].ChartType = SeriesChartType.Candlestick;
+                    break;
+                case 6:
+                    chart1.Series[0].ChartType = SeriesChartType.Bar;
+                    break;
+                case 7:
+                    chart1.Series[0].ChartType = SeriesChartType.Doughnut;
+                    break;
+                case 8:
+                    chart1.Series[0].ChartType = SeriesChartType.Funnel;
+                    break;
+                case 9:
+                    chart1.Series[0].ChartType = SeriesChartType.Kagi;
+                    break;              
+                case 10:
+                    chart1.Series[0].ChartType = SeriesChartType.Point;
+                    break;
+                case 11:
+                    chart1.Series[0].ChartType = SeriesChartType.FastLine;
+                    break;
+            }
+        }
+    }
+}
